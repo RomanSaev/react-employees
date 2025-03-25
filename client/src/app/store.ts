2cb5d1880e9936a1e5c6b604f1bd2f3cd8aa7d97
Roman <romanfromvlg@gmail.com>
2025-03-26 00:02:56 +0300

    Создан слайс employees, добавлен в стор. В слайсе привязан пока только эндпоинт для получения всех сотрудников.

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,6 +1,7 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import auth from '../features/auth/authSlice'
+import employees from '../features/employees/employeesSlice'
 import { api } from "./services/api"
 import { listenerMiddleware } from "../middleware/auth"
 
@@ -11,7 +12,8 @@ export type RootState = ReturnType<typeof store.getState>
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    auth
+    auth,
+    employees,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware);
